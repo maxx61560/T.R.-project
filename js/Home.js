bd74b1b2dc03ac2c8fe00df4e96836abdd01edb5
Max Vilà Ruiz <max16506@gmail.com>
2023-10-23 14:14:06 +0200

    Table Style

--- js/Home.js
+++ js/Home.js
@@ -1,8 +1,8 @@
 //To put the name of the user in the 'Home' welcome.
+
 const span_name = document.querySelector('#span_name');
 const valorLocalStorage = localStorage.getItem('user_name');
 
-//Add name of Log in prefils.
 if(valorLocalStorage){
     span_name.textContent = valorLocalStorage;
 } else{
@@ -11,57 +11,66 @@ if(valorLocalStorage){
 
 
 
-//Code of the main function (to create event):
+//Code of the main function (table):
+
+//Default event:
+const defaultEvent = {};
+defaultEvent["difficulty"] = "Fàcil";
+defaultEvent["name"] = "Exemple d'esdeveniment";
+defaultEvent["date"] = "dd/mm/aaaa";
+InsertData(defaultEvent);
+
+// To create events:
 var Row = null
 function onSubmit() {
-        let DataForm = Read()
-        if (Row == null){
-            InsertData(DataForm)
+        let DataForm = Read();
+        if (Row === null){
+            InsertData(DataForm);
         } else{
-            Update(DataForm)
-            Empty()
+            Update(DataForm);
+            Empty();
     }
 }
 function Read() {
-    let DataForm = {}
-    DataForm["name"] = document.getElementById("name").value
-    DataForm["date"] = document.getElementById("date").value
-    DataForm["difficulty"] = document.getElementById("difficulty").value
-    return DataForm
+    let DataForm = {};
+    DataForm["difficulty"] = document.getElementById("difficulty").value;
+    DataForm["name"] = document.getElementById("name").value;
+    DataForm["date"] = document.getElementById("date").value;
+    return DataForm;
 }
 function InsertData(data) {
-    let Board = document.getElementById("board").getElementsByTagName('tbody')[0]
-    let Row = Board.insertRow(Board.length)
-    column1 = Row.insertCell(0).innerHTML = data.name
-    column2 = Row.insertCell(1).innerHTML = data.date
-    column3 = Row.insertCell(2).innerHTML = data.difficulty
-    column3 = Row.insertCell(3).innerHTML = `<input class="submit" type="button" onClick="Edit(this)" value="Edit" >
-                                            <input class="submit" type="button" onClick="Delete(this)" value="Delete" >`
-    document.getElementById("name").focus()
-    Empty()
+    let Table = document.getElementById("table").getElementsByTagName('tbody')[0];
+    let Row = Table.insertRow(Table.length);
+    column1 = Row.insertCell(0).innerHTML = data.difficulty;
+    column2 = Row.insertCell(1).innerHTML = data.name;
+    column3 = Row.insertCell(2).innerHTML = data.date;
+    column3 = Row.insertCell(3).innerHTML = `<input class="submit optionsTable_home edit_home" type="button" onClick="Edit(this)" value="Edit" >
+                                            <input class="submit optionsTable_home delete_home" type="button" onClick="Delete(this)" value="Delete" >`;
+    document.getElementById("difficulty").focus();
+    Empty();
 }
 function Empty() {
-    document.getElementById("name").value = ""
-    document.getElementById("date").value = ""
-    document.getElementById("difficulty").value = ""
-    Row = null
+    document.getElementById("difficulty").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("date").value = "";
+    Row = null;
 }
 function Edit(td) {
-    Row = td.parentElement.parentElement
-    document.getElementById("name").value = Row.cells[0].innerHTML
-    document.getElementById("date").value = Row.cells[1].innerHTML
-    document.getElementById("difficulty").value = Row.cells[2].innerHTML
+    Row = td.parentElement.parentElement;
+    document.getElementById("difficulty").value = Row.cells[0].innerHTML;
+    document.getElementById("name").value = Row.cells[1].innerHTML;
+    document.getElementById("date").value = Row.cells[2].innerHTML;
 }
 function Update(DataForm) {
-    Row.cells[0].innerHTML = DataForm.name
-    Row.cells[1].innerHTML = DataForm.date
-    Row.cells[2].innerHTML = DataForm.difficulty
-    document.getElementById("name").focus()
+    Row.cells[0].innerHTML = DataForm.difficulty;
+    Row.cells[1].innerHTML = DataForm.name;
+    Row.cells[2].innerHTML = DataForm.date;
+    document.getElementById("difficulty").focus();
 }
 function Delete(td) {
-    if (confirm('¿Seguro de borrar este registro?')) { //Cambiar mensaje
-        Row = td.parentElement.parentElement
-        document.getElementById("board").deleteRow(Row.rowIndex)
-        Empty()
+    if (confirm('Segur que vols eliminar aquest esdeveniment?')) {
+        Row = td.parentElement.parentElement;
+        document.getElementById("table").deleteRow(Row.rowIndex);
+        Empty();
     }
 }
--- js/Register.js
+++ js/Register.js
@@ -16,23 +16,29 @@ submit.addEventListener('click', function (e) {
     let password = document.querySelector('#password');
     let password2 = document.querySelector('#password2');
     let result = `, ['${email.value}', '${password.value}', '${name.value}']`;
-    for(let i = 0; i < database.length; i++){
-        if(email.value !== database[i][0]){
-            if(password.value === password2.value){
-                alert(`Perfecte! Per acabar, envia la següent informació al correu electrònic 'maxvilaruiz123@gmail.com': \n\n${result}`);
-                localStorage.setItem('user_name', name.value); //To put the name of the user in the 'Home' welcome.
-                window.location.href = 'Home.html';
-                break;
+
+    if(name.value !== '' & email.value !== '' & password.value !== '' & password2.value !== ''){ //To make sure all the inputs have values.
+        for(let i = 0; i < database.length; i++){ 
+            if(email.value !== database[i][0]){ //To find if the email is already being used.
+                if(password.value === password2.value){ //To confirm that both passwords are equals.
+                    alert(`Perfecte! Per acabar, envia la següent informació al correu electrònic 'maxvilaruiz123@gmail.com': \n\n${result}`);
+                    localStorage.setItem('user_name', name.value); //To put the name of the user in the 'Home' welcome.
+                    window.location.href = 'Home.html';
+                    break;
+                } else{
+                    verify_pw = true;
+                }
             } else{
-                verify_pw = true;
+                verify_email = true;
             }
-        } else{
-            verify_email = true;
         }
-    }
-    if(verify_email){
-        alert("Aquest correu electrònic ja està en ús, si us plau prova amb un altre.");
-    } else if(verify_pw){
-        alert('Les contrasenyes no coincideixen, si us plau torna-ho a intentar.');
+    
+        if(verify_email){
+            alert("Aquest correu electrònic ja està en ús, si us plau prova amb un altre.");
+        } else if(verify_pw){
+            alert('Les contrasenyes no coincideixen, si us plau torna-ho a intentar.');
+        }
+    } else{
+        alert('Has de completar tots els camps.');
     }
 })
